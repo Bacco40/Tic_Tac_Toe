2303e40846fae5e85ad487ed8cabd692cedf252d
Bacco40 <bacchettimatteo@gmail.com>
2022-01-04 18:49:00 +0100

    created the base of the page and the graphics

---
+++ script.js
@@ -0,0 +1,9 @@
+const game = ((name1,name2,simbol1,simbol2)=>{
+    const gameboard={};
+    const player=(name,simbol)=>{
+        this.name=name;
+        this.simbol=simbol;
+    }
+    const player1=new player(name1,simbol1);
+    const player2=new player(name2,simbol2);
+})();
